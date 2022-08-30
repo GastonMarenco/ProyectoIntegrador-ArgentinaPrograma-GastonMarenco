3e19c6259a17f51fbfd9c9f0c505273d0a5d2058
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2022-08-30 01:30:46 -0300

    Creación del BackEnd

---
+++ BackEnd/src/test/java/com/portfolio/PortafilioAP/PortafilioApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortafilioAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafilioApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
