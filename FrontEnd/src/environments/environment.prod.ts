daecfc821a52ab45831b319574ed638dc398e6df
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2022-11-28 17:17:35 -0300

    Cambio de Heroku a Koyeb

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const environment = {
     messagingSenderId: '1059902260744',
   },
   production: true,
-  URL: 'https://backend-ap-gm.herokuapp.com/'
+  URL: 'proyectointegrador-argentinaprograma-gastonmarenco.portfolio.koyeb'
 };
